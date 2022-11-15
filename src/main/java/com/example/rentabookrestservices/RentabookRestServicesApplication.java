9f293ce3316945cab4f1335aa1f97e2f0239d60c
abmehmet <mehmetercan5755@gmail.com>
2022-11-15 09:05:59 +0300

    first comment for rest services

---
+++ src/main/java/com/example/rentabookrestservices/RentabookRestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentabookrestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentabookRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentabookRestServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rentabookrestservices/RentabookRestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentabookrestservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentabookRestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
