d9b60f2d0e1ad8ff203b40369cd2674e4e959a39
abmehmet <mehmetercan5755@gmail.com>
2022-11-16 10:25:09 +0300

    post işleminde, Book nesnesi içinde gelen BS (BookSpecification), önce kaydedilip daha sonra Book kaydedildi

--- src/main/java/com/example/rentabookrestservices/controller/BookController.java
+++ src/main/java/com/example/rentabookrestservices/controller/BookController.java
@@ -3,36 +3,40 @@ package com.example.rentabookrestservices.controller;
 import com.example.rentabookrestservices.domain.Book;
 import com.example.rentabookrestservices.exception.BookNotFoundException;
 import com.example.rentabookrestservices.repository.BookRepository;
+import com.example.rentabookrestservices.repository.BookSpecificationRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class BookController {
-    private final BookRepository repository;
+    private final BookRepository bookRepository;
+    private final BookSpecificationRepository bookSpecificationRepository;
 
-    BookController(BookRepository repository) {
-        this.repository = repository;
+    BookController(BookRepository bookRepository, BookSpecificationRepository bookSpecificationRepository) {
+        this.bookRepository = bookRepository;
+        this.bookSpecificationRepository = bookSpecificationRepository;
     }
 
     @GetMapping("/books")
     List<Book> getAllBooks() {
-        return repository.findAll();
+        return bookRepository.findAll();
     }
 
     @GetMapping("/books/{id}")
     Book getBook(@PathVariable Long id) {
-        return repository.findById(id)
+        return bookRepository.findById(id)
                 .orElseThrow(() -> new BookNotFoundException(id));
     }
 
     @PostMapping("/books")
     Book newBook(@RequestBody Book newBook) {
-        return repository.save(newBook);
+        bookSpecificationRepository.save(newBook.getBookSpecification());
+        return bookRepository.save(newBook);
     }
 
     @DeleteMapping("/books/{id}")
     void deleteBook(@PathVariable Long id) {
-        repository.deleteById(id);
+        bookRepository.deleteById(id);
     }
 }
